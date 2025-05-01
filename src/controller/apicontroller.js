35133a9f9f6df6e99c916a82e73160f2ce9a45da
rjoelgs <rjoelgs@hotmail.com>
2025-04-30 22:15:45 -0600

    mas corregir

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -1,19 +1,16 @@
 const verificar = (req, res) => {
-
     try {
         let tokenCode = "UNDERTOKEN";
-        let token = req.query("hub.verify_token");
-        let challenge = req.query("hub.challenge");
+        let token = req.query["hub.verify_token"];
+        let challenge = req.query["hub.challenge"];
 
-        if (challenge != null && token != null && token == tokenCode) {
+        if (challenge != null && token != null && token === tokenCode) {
             res.send(challenge);
         } else {
             res.send("Error");
         }
-        
 
         console.log(req);
-        
     } catch (error) {
         console.log(error);
         res.status(500).send("Error al verificar");
@@ -25,6 +22,8 @@ const recibir = (req, res) => {
     res.send("recibir");
 };
 
-module.exports = {verificar, recibir};
+module.exports = { verificar, recibir };
+
+
 
 
