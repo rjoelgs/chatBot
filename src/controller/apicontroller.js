8c068498ad0b6fde6398de3a380da4a0672e378d
rjoelgs <rjoelgs@hotmail.com>
2025-05-05 16:57:10 -0600

    concatenando

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -22,8 +22,12 @@ const recibir = (req, res) => {
     try{
         let entry = req.body.entry[0];
         let changes = entry.changes[0];
-        
-        console.log(changes);
+        let value = changes.value;
+        let message = value.message;
+        let from = message.from;
+
+        console.log(from);
+        console.log(message);
         res.send("EVENT_RECEIVED");
     }catch(e){
         console.log(e);
